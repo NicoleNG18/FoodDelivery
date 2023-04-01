a5ee8eca5e341aa89f4aed2aab59f7f8e06082e7
Nikol Georgieva <nicole.georgieva18@abv.bg>
2023-04-01 14:32:45 +0300

    Html and css changes

--- src/main/java/bg/softuni/fooddelivery/web/HomeController.java
+++ src/main/java/bg/softuni/fooddelivery/web/HomeController.java
@@ -1,13 +1,21 @@
 package bg.softuni.fooddelivery.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String getHome(){
+    public String getHome(Principal principal,
+                          Model model) {
+        if (principal != null) {
+            model.addAttribute("name", principal.getName());
+        }
+
         return "index";
     }
 }
